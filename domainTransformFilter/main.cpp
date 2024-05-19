e36c409a8011210a25b3a098edcecb4cd10a7527
giacomo carsana <giacomocarsana@digitecinnovation.com>
2024-05-19 09:54:03 +0200

    update main

--- domainTransformFilter/main.cpp
+++ domainTransformFilter/main.cpp
@@ -192,78 +192,6 @@ cv::Mat WL(cv::Mat& sc,int L, int H)
 	return r;
  }
 
-cv::Mat detailEnhancement(Mat& src)
-{
-	cp::MultiScaleBilateralFilter bf;
-	bf.setPyramidComputeMethod(cp::MultiScaleFilter::OpenCV);
-
-	string sname = "smoothed";
-	string wname = "detail enhancement";
-	namedWindow(wname);
-	namedWindow(sname);
-	
-	int sc = 25;
-	int ss = 170;
-	int iteration = 2;
-	int boost = 430;
-	int L = 300; 
-	int H = 1300;
-	
-	string wccname = "cw";
-	namedWindow(wccname);
-	createTrackbar("s_color",wccname,&sc,1000);
-	createTrackbar("s_space",wccname,&ss,1000);
-	createTrackbar("iteration",wccname,&iteration,2);
-	createTrackbar("boost",wccname,&boost,500);
-	createTrackbar("L",wccname,&L,1000);
-	createTrackbar("H",wccname,&H,2000);
-	
-	int key = 0;
-	double zoomLevel = 1.0;
-
-	Mat show;
-	Mat smooth;
-	Mat sub;
-	Mat bfDst;
-
-	while(key!='q' && key!=VK_ESCAPE)
-	{
-		if (key == '+') {
-			zoomLevel += 0.1;
-		}
-		// Decrease zoom level when '-' key is pressed
-		else if (key == '-') {
-			zoomLevel -= 0.1;
-			if (zoomLevel < 0.1) zoomLevel = 0.1;
-		}
-
-		int64 startTime = getTickCount();
-
-		domainTransformFilter(src, smooth,ss,sc,iteration,DTF_L1,DTF_RF,DTF_BGRA_SSE_PARALLEL);
-		//ximgproc::dtFilter(src, src, smooth, ss, sc, 0, iteration);
-
-		//subtract(src,smooth,sub,noArray(),CV_32F);
-		//sub*=(boost*0.1);
-		//add(src,1*sub,sub,noArray(),CV_32F);
-		
-		cv::normalize(src, bfDst, 250, 0, NORM_MINMAX);
-
-		bf.filter(src, bfDst, sc, ss, boost/100, 8);
-		double time = (getTickCount()-startTime)/(getTickFrequency());
-		printf("domain transform filter with enhancement: %f ms\n",time*1000.0);
-
-		displayWithZoom(WL(src, L, H), zoomLevel, wname);
-		displayWithZoom(WL(bfDst, L, H), zoomLevel, sname);
-		
-		//imshow(wname,show);
-		imshow(wccname,Mat::zeros(cv::Size(800,200), CV_8U));
-		key = waitKey(100);
-	}
-
-	cv::destroyWindow(wname);
-	cv::destroyWindow(wccname);
-	return bfDst;
-}
 
 void drawText(std::string text, cv::Mat image)
 {
@@ -276,6 +204,23 @@ void drawText(std::string text, cv::Mat image)
 	cv::putText(image, text, textOrg, fontFace, fontScale, color, thickness);
 }
 
+
+
+cv::Mat removeEdge(cv::Mat& src, double minAnatomicLevel = 100., int erodeSize = 5)
+{
+	cv::Mat anatomicMask;
+	cv::threshold(src, anatomicMask, minAnatomicLevel, 1., cv::ThresholdTypes::THRESH_BINARY_INV);
+	anatomicMask.convertTo(anatomicMask, CV_8U);
+
+	cv::Mat kernel = cv::Mat::ones(cv::Size(erodeSize, erodeSize), CV_32F);
+	cv::Mat rc;
+	cv::morphologyEx(anatomicMask, anatomicMask, cv::MorphTypes::MORPH_DILATE, kernel);
+	src.copyTo(rc);
+	rc.setTo(0, anatomicMask);
+	return rc;
+}
+
+
 std::vector<cv::Mat>  multiFileDetailEnhancement(std::vector<cv::Mat>& src)
 {
 	string sname = "smoothed";
@@ -284,30 +229,20 @@ std::vector<cv::Mat>  multiFileDetailEnhancement(std::vector<cv::Mat>& src)
 	namedWindow(sname);
 
 	cp::MultiScaleBilateralFilter bf;
-#if 1	
 	bf.setPyramidComputeMethod(cp::MultiScaleFilter::OpenCV);
-	int sc = 40;
-	int ss = 170;
-	int boost = 700;
-	int L = 170;
-	int H = 1500;
+
+	int sigma_range = 300;
+	int ss = 0;
+	int boost = 45;
+	int L = 80;
+	int H = 320;
 	int level = 6;
 	cp::MultiScaleFilter::ScaleSpace scalespaceMethod = cp::MultiScaleFilter::ScaleSpace::Pyramid;
-#else
-	// bf.setPyramidComputeMethod(cp::MultiScaleFilter::OpenCV);
-	int sc = 40;
-	int ss = 1;
-	int boost = 700;
-	int L = 170;
-	int H = 1500;
-	int level = 4;
-	cp::MultiScaleFilter::ScaleSpace scalespaceMethod = cp::MultiScaleFilter::ScaleSpace::DoG;
-#endif
 
 	string wccname = "cw";
 	namedWindow(wccname);
-	createTrackbar("s_color", wccname, &sc, 1000);
-	createTrackbar("s_space", wccname, &ss, 1000);
+	createTrackbar("sigma_range", wccname, &sigma_range, 2000);
+	createTrackbar("level", wccname, &level, 9);
 	createTrackbar("boost", wccname, &boost, 1000);
 	createTrackbar("L", wccname, &L, 1000);
 	createTrackbar("H", wccname, &H, 2000);
@@ -315,9 +250,6 @@ std::vector<cv::Mat>  multiFileDetailEnhancement(std::vector<cv::Mat>& src)
 	int key = 0;
 	double zoomLevel = 1.0;
 
-	Mat show;
-	Mat smooth;
-	Mat sub;
 	Mat bfDst;
 	int nPos = 0;
 
@@ -340,31 +272,43 @@ std::vector<cv::Mat>  multiFileDetailEnhancement(std::vector<cv::Mat>& src)
 				nPos = 0;
 			break;
 
-		case 'm':			
+		case 'm':
 			nPos -= 1;
 			if (nPos < 0)
-				nPos = src.size()-1;
+				nPos = src.size() - 1;
 			break;
 		}
 
+		double minAnatomicLevel = 100.;
+		int erodeSize{ 5 };
+
+		cv::Mat img;
+		src[nPos].copyTo(img);
+		img = removeEdge(img, minAnatomicLevel, erodeSize);
+
 
 		int64 startTime = getTickCount();
-
-		cv::normalize(src[nPos], bfDst, 250, 0, NORM_MINMAX);
-		
-
-		bf.filter(src[nPos], bfDst, sc, ss, boost / 100, level, scalespaceMethod);
+		bf.filter(src[nPos], bfDst, sigma_range, ss, boost / 10, level, scalespaceMethod);
 		double time = (getTickCount() - startTime) / (getTickFrequency());
 		printf("domain transform filter with enhancement: %f ms\n", time * 1000.0);
 
-		
-		drawText("Frame " + std::to_string(nPos) + "\\" + std::to_string(src.size()), bfDst);
-
-		displayWithZoom(WL(src[nPos], L, H), zoomLevel, wname);
-		displayWithZoom(WL(bfDst, L, H), zoomLevel, sname);
 
 
-		//imshow(wname,show);
+		cv::Mat srcScaled;
+		src[nPos].copyTo(srcScaled);
+		srcScaled /= 50.f;
+		rotate(srcScaled, srcScaled, cv::ROTATE_90_CLOCKWISE);
+		flip(srcScaled, srcScaled, 1);
+		displayWithZoom(WL(srcScaled, L, H), zoomLevel, wname);
+
+		cv::Mat dstbfDst;
+		bfDst.copyTo(dstbfDst);
+		dstbfDst /= 50.f;
+		rotate(dstbfDst, dstbfDst, cv::ROTATE_90_CLOCKWISE);
+		flip(dstbfDst, dstbfDst, 1);
+		drawText("Frame " + std::to_string(nPos) + "\\" + std::to_string(src.size()), dstbfDst);
+		displayWithZoom(WL(dstbfDst, L, H), zoomLevel, sname);
+
 		imshow(wccname, Mat::zeros(cv::Size(800, 200), CV_8U));
 		key = waitKey(100);
 	}
@@ -375,62 +319,13 @@ std::vector<cv::Mat>  multiFileDetailEnhancement(std::vector<cv::Mat>& src)
 	for (auto const& s : src)
 	{
 		cv::Mat r;
-		bf.filter(s, r, sc, ss, boost / 100, level, scalespaceMethod);
+		bf.filter(s, r, sigma_range, ss, boost / 10, level, scalespaceMethod);
 		d.push_back(r);
 	}
 	return d;
 }
 
 
-void bpdetailEnhancement(Mat& src)
-{
-	string wname = "detail enhancement";
-	string wccname = "cws";
-	namedWindow(wname);
-	namedWindow(wccname);
-
-	int sc = 20;
-	int ss = 70;
-	int sclp = 5;
-	int sslp = 6;
-	int iteration = 2;
-	int boost = 30;
-	createTrackbar("slp_color", wccname, &sclp, 255);
-	createTrackbar("slp_space", wccname, &sslp, 255);
-	createTrackbar("s_color", wccname, &sc, 255);
-	createTrackbar("s_space", wccname, &ss, 255);
-	createTrackbar("iteration", wccname, &iteration, 255);
-	createTrackbar("boost", wccname, &boost, 500);
-	int key = 0;
-
-	Mat show;
-	Mat smooth;
-	Mat sub;
-	Mat srcLp;
-	while (key != 'q' && key != VK_ESCAPE)
-	{
-		int64 startTime = getTickCount();
-
-		domainTransformFilter(src, srcLp, sslp, sclp, iteration, DTF_L1, DTF_RF, DTF_BGRA_SSE_PARALLEL);
-
-		domainTransformFilter(srcLp, smooth, ss, sc, iteration, DTF_L1, DTF_RF, DTF_BGRA_SSE_PARALLEL);
-
-		subtract(srcLp, smooth, sub, noArray(), CV_32F);
-		sub *= (boost * 0.1);
-		add(srcLp, 1 * sub, sub, noArray(), CV_32F);
-		sub.convertTo(show, CV_8U);
-
-		double time = (getTickCount() - startTime) / (getTickFrequency());
-		printf("domain transform filter with enhancement: %f ms\n", time * 1000.0);
-
-		imshow(wname, show);
-		imshow(wccname, Mat::zeros(cv::Size(800, 200), CV_8U));
-		key = waitKey(1);
-	}
-
-	destroyWindow(wname);
-	destroyWindow(wccname);
-}
 
 std::vector<std::string> processFolder(const std::string& folder_path)
 {
@@ -448,23 +343,6 @@ std::vector<std::string> processFolder(const std::string& folder_path)
 
 
 
-
-
-cv::Mat removeEdge(cv::Mat& src, double minAnatomicLevel = 100.)
-{
-	cv::Mat anatomicMask;
-	cv::threshold(src, anatomicMask, minAnatomicLevel, 1., cv::ThresholdTypes::THRESH_BINARY_INV);
-	anatomicMask.convertTo(anatomicMask, CV_8U);
-
-	cv::Mat kernel = cv::Mat::ones(cv::Size(5, 5), CV_32F);
-	cv::Mat rc;
-	cv::morphologyEx(anatomicMask, anatomicMask, cv::MorphTypes::MORPH_DILATE, kernel);
-	src.copyTo(rc);
-	rc.setTo(0, anatomicMask);
-	return rc;
-}
-
-
 void enhFolder(const std::string& folder_path, const std::string& dest_path)
 {
 	std::vector<std::string> allFiles = processFolder(folder_path);
@@ -472,14 +350,10 @@ void enhFolder(const std::string& folder_path, const std::string& dest_path)
 	for (const auto &v: allFiles )
 	{ 
 		Mat img = imread(v, IMREAD_ANYDEPTH | IMREAD_GRAYSCALE);
-		double minAnatomicLevel = 100.;
-		img = removeEdge(img, minAnatomicLevel);
+		//double minAnatomicLevel = 100.;
+		//img = removeEdge(img, minAnatomicLevel);
 	
-		/*rotate(img, img, cv::ROTATE_90_CLOCKWISE);
-		flip(img, img, 1);*/
-
 		img.convertTo(img, CV_32F);
-		cv::normalize(img, img, 1000, 0, NORM_MINMAX);
 		src.push_back(img);
 	}
 	
@@ -503,38 +377,24 @@ int main(int argc, char** argv)
 {
 	std::string inputImgOrFolder = ".\\inData";
 	std::string outputImgOrFolder = ".\\outData";
+	float sigma_range{ 4.5f };
+	int pyrLevels{ 6 };
+	bool bUI{ false };
+
 	std::string filterType;
-	float fsigma{ 0.f }, fFiltAlpha{ 0.f };
-	int halfPatch, halfSearchWindow, nTemporalFrames{ 1 }, nProcFrames{ 1 }, nSkipFrames{ 0 }, pyrLevels{ 0 };
 	int halfWienerBlock{ 3 };
-	int iteration{ 1 };
 	float sigmaWiener{ 0.25f };
-	bool useXCorr{ false };
-	bool bGaussian{ false };
-	bool bVST{ true };
 
 	// Declare the supported options.
 	po::options_description desc("allowed options");
 	desc.add_options()
 		("help,h", "produce help message")
 		("input,i", po::value<std::string>(&inputImgOrFolder), "input image folder")
-		("type,t", po::value<std::string>(&filterType), "filter type (dct, nlm, nlmMS, nlmMSW)")
-		("sigma,s", po::value<float>(&fsigma), "noise std")
-		("alpha,a", po::value<float>(&fFiltAlpha), "filter alpha")
-		("patchSize,p", po::value<int>(&halfPatch), "half patch size")
-		("iterate,j", po::value<int>(&iteration), "iteration counts")
-		("searchSize,w", po::value<int>(&halfSearchWindow), "half search wnd size")
+		("out,o", po::value<std::string>(&outputImgOrFolder), "output image folder")
 		("pyrLevels,y", po::value<int>(&pyrLevels), "pyramid levels")
-		("gaussian,g", po::value<bool>(&bGaussian), "gaussian weights")
-		("vst,v", po::value<bool>(&bVST), "variance stabilization")
-		("tFrame,f", po::value<int>(&nTemporalFrames), "temporal frame number")
-		("wienerHalfBlock,b", po::value<int>(&halfWienerBlock), "wiener half block size")
-		("wienerSigma,c", po::value<float>(&sigmaWiener), "wiener noise sigma")
-		("skipFrames,k", po::value<int>(&nSkipFrames), "skip frames")
-		("xCorr,x", po::value<bool>(&useXCorr), "use correlation (fixed -> tbd)")
-		("transientMode,u", "produce frames during settling time")
-		("outFrames,n", po::value<int>(&nProcFrames), "processed output frames")
-		("out,o", po::value<std::string>(&outputImgOrFolder), "output image folder");
+		("sigma,s", po::value<float>(&sigma_range), "sigma MSE gaussian")
+		("ui,g", po::value<bool>(&bUI), "show UI");
+
 
 	po::variables_map vm;
 	try
@@ -570,26 +430,6 @@ int main(int argc, char** argv)
 	}
 
 	enhFolder(inputImgOrFolder, outputImgOrFolder);
-
-	/*Mat img = imread("s_0024.png", IMREAD_ANYDEPTH | IMREAD_GRAYSCALE);
-	
-	cv::Mat anatomicMask;
-	cv::threshold(img, anatomicMask, 100, 1, cv::ThresholdTypes::THRESH_BINARY_INV);
-	anatomicMask.convertTo(anatomicMask, CV_8U);
-
-	cv::Mat rc;
-	cv::Mat kernel = cv::Mat::ones(cv::Size(15, 15),CV_32F);
-	cv::morphologyEx(anatomicMask, anatomicMask, cv::MorphTypes::MORPH_DILATE, kernel );
-	img.setTo(0, anatomicMask);
-
-	rotate(img, rc, cv :: ROTATE_90_CLOCKWISE);
-	flip(rc, rc, 1);
-	
-	rc.convertTo(rc, CV_32F);
-	cv::normalize(rc, rc, 1000, 0, NORM_MINMAX);
-	cv::Mat r = detailEnhancement(rc);
-	cv::imwrite("s_0024Filt.tif", r);*/
-
 
 	return 0;
 }
